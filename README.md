a04332edfda1a40b8b04d42c28fd710b85176acd
Tak <takeshiseodev@gmail.com>
2023-09-25 20:06:40 -0400

    added ReactLimitations.jsx

--- README.md
+++ README.md
@@ -4,11 +4,25 @@ A curated list of React interview questions with explanations, solutions, and va
 
 ## Table of Contents
 
+1. [Virtual DOM and the "real" DOM](#virtual-dom-and-the-real-dom)
+2. [React Limitations](#react-limitations)
+
+## Virtual DOM and the "real" DOM
+
 1. [What is the difference between the Virtual DOM and the "real" DOM?](./VirtualVsRealDOM.jsx) - Delve into the distinctions between the Virtual DOM and the actual browser's DOM.
 
    - [Vitual DOM vs Real DOM](images/VirtualDOM.png)
 
    - [React Docs](https://legacy.reactjs.org/docs/faq-internals.html#what-is-the-virtual-dom)
+
+## React Limitations
+
+2. [What are the limitations of React?](./ReactLimitations.jsx) - Explore the "limitations" of React.
+   - **It is a library, not a framework**: While this allows for more flexibility, it also means developers may need to integrate other libraries for state management, routing, etc.
+   - **It is large**: The size of the library can be an issue for performance, especially for applications that need to be lightweight.
+   - **Being owned by Facebook**: Some organizations might have reservations using a library owned by a large corporation with its licensing terms.
+   - **Documentation**: While extensive, React's documentation can sometimes be overwhelming or lack specific details on certain topics.
+
 
 
 
---
+++ ReactLimitations.jsx
@@ -0,0 +1,27 @@
+/**
+ * React Limitations
+ * -----------------
+ * Not necessarily a bad thing but something you want to be aware of in the interview process.
+ *
+ * - It is a library, not a framework
+ *   - This doesn’t mean it's a good or bad thing, but there are some pros and cons.
+ *   - Library enhances your existing code. You could write a whole JavaScript application and 
+ *     incorporate React into parts of it. It was a big selling point when React first emerged.
+ *   - Being a library, it lets developers roam free. Some need a framework, hence we have 
+ *     Gatsby, Next.js, etc., which are built on top of React.
+ *
+ * - It is large
+ *   - This isn’t necessarily bad, especially if you have code splitting and certain best 
+ *     practices in place.
+ *   - There are smaller versions like Preact which function similarly but are lighter.
+ *
+ * - Being owned by Facebook
+ *   - Pros: Maintained by a large corporation, ensuring its longevity. Facebook developers 
+ *     have salaries, so even though it's open source, a team within Facebook makes major 
+ *     decisions.
+ *   - Cons: Privacy and transparency concerns. Trust issues with Facebook's privacy? 
+ *     Communication struggles with the internal team have been reported, despite React being open source.
+ *
+ * - Documentation
+ *   - It's not as up-to-date as it should be.
+ */
